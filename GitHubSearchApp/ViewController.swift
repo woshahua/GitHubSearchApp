82b8f72705c9aa8df38b54df9b1961db25b8d884
hang.gao <hang.gao@dena.com>
2021-12-14 12:27:06 +0900

    Initial Commit

---
+++ GitHubSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubSearchApp
+//
+//  Created by Hang Gao on 2021/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
